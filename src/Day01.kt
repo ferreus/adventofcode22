30a9de13926822f5a747abeffec5b4ee68ae1dfa
Vladimir Vainer <vladimir.v@everysight.com>
2022-12-01 13:55:45 +0200

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val elfs = mutableListOf<Int>()
+        var current = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                if (current>0) {
+                    elfs.add(current)
+                    current = 0
+                }
+            } else {
+                current += line.toInt()
+            }
+        }
+
+        return elfs.maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfs = mutableListOf<Int>()
+        var current = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                if (current>0) {
+                    elfs.add(current)
+                    current = 0
+                }
+            } else {
+                current += line.toInt()
+            }
+        }
+        return elfs.sorted().takeLast(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
